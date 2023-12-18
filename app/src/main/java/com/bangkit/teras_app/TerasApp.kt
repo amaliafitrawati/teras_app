2784334f0efa8a1120cd22af2cc757abcd232390
amaliafitrawati <amaliafitrawati13@gmail.com>
2023-12-19 00:25:09 +0700

    add top app bar for board and story

--- app/src/main/java/com/bangkit/teras_app/TerasApp.kt
+++ app/src/main/java/com/bangkit/teras_app/TerasApp.kt
@@ -2,16 +2,23 @@ package com.bangkit.teras_app
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.bangkit.teras_app.data.topAppBarLabel
 import com.bangkit.teras_app.ui.components.BottomBar
 import com.bangkit.teras_app.ui.navigation.Screen
 import com.bangkit.teras_app.ui.screen.account.AccountScreen
@@ -20,17 +27,35 @@ import com.bangkit.teras_app.ui.screen.board.BoardScreen
 import com.bangkit.teras_app.ui.screen.home.HomeScreen
 import com.bangkit.teras_app.ui.screen.story.BrowseStoryScreen
 
+@Composable
+fun currentRoute(navController: NavHostController): String? {
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    return navBackStackEntry?.destination?.route
+}
+
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TerasApp(
     modifier: Modifier = Modifier,
     navController: NavHostController = rememberNavController(),){
+    val currentRoute = currentRoute(navController).toString()
     Scaffold(
+        topBar = {
+            if (currentRoute !in listOf("home", "account")) {
+                CenterAlignedTopAppBar(
+                    title = {
+                        Text(text = stringResource(topAppBarLabel(screenName = currentRoute)),
+                            style = MaterialTheme.typography.headlineSmall,)
+                    },
+                    )
+            }
+        },
         bottomBar = {
             BottomBar(navController,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .graphicsLayer{
+                    .graphicsLayer {
                         shadowElevation = 50f
                     }
             )
